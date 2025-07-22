efbcfd5cf68d43b5b5420949559b2e3beae6d9e7
malatesh30 <malateshnayak30.com>
2025-07-22 13:48:08 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const productRoutes = require("./routes/productConfigRoutes");
+const configRoutes = require("./routes/productConfigRoutes");
 
 const app = express();
 app.use(express.json());
-app.use("/api", productRoutes);
+app.use("/api", configRoutes);
 
-mongoose.connect(process.env.url)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+mongoose.connect("mongodb://localhost:27017/product-configurator", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error("MongoDB error:", err));
 
 module.exports = app;
